e72303bbe551935b1a277a4db5db8615e074d92e
buenocjm <cbpunko35@gmail.com>
2023-05-28 18:19:15 -0400

    commit

---
+++ first_program_go.go
@@ -0,0 +1,11 @@
+/*
+podemos describir lo que hace cada segmento de codigo en una seccion como esta
+*/
+package main
+
+import "fmt"
+
+func main() {
+	myName := "Cristian Bueno" //put your name
+	fmt.Println("Hello ", myName)
+}
